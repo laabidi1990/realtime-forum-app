d5090df5e2be867b260b63e0e9c7ef27be5f61c6
Laabidi <mabidi1990@gmail.com>
2020-06-04 18:29:12 +0200

    implementation of vue, vuetify, vue-router / creation of toolbar and footer components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+
+      <v-toolbar-title>Realtime Forum</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+        <v-btn class="ma-5" tile color="indigo" dark>Forum</v-btn>
+        <v-btn class="ma-2" tile outlined color="indigo">Ask a Question</v-btn>
+        <v-btn class="ma-5" tile color="indigo" dark>Login</v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+    </v-toolbar>
+  </v-card>
+      
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
